c70a3c577c2ac2a4a9da693c3346bd613a380669
Merlintor <merlinfuchs2001@gmail.com>
2020-02-06 18:07:30 +0100

    minor

--- xenon_worker/commands/context.py
+++ xenon_worker/commands/context.py
@@ -6,7 +6,7 @@ class Context:
     def get_channel(self):
         return self.client.get_channel(self.msg.guild_id, self.msg.channel_id)
 
-    async def get_guild(self):
+    def get_guild(self):
         return self.client.get_guild(self.msg.guild_id)
 
     def send(self, *args, **kwargs):
