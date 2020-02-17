9d72906abe788316baeef84802defddcedde0ad7
Merlintor <merlinfuchs2001@gmail.com>
2020-02-17 15:52:53 +0100

    Add icons to default formats

--- discord_worker/commands/formatter.py
+++ discord_worker/commands/formatter.py
@@ -10,12 +10,37 @@ class Format:
 
 class Formatter:
     DEFAULT = Format()
-    INFO = Format(title="Info", color=0x3db1ff)
-    SUCCESS = Format(title="Success", color=0x3dff91)
-    WARNING = Format(title="Warning", color=0xffd23d)
-    ERROR = Format(title="Error", color=0xff5a3d)
-    WORKING = Format(title="Please Wait ...")
-    WAITING = Format(title="Waiting for Input ...")
+    INFO = Format(
+        title="Info",
+        color=0x478fce,
+        icon="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678110-sign-info-512.png"
+    )
+    SUCCESS = Format(
+        title="Success",
+        color=0x48ce6c,
+        icon="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678134-sign-check-512.png"
+    )
+    WARNING = Format(
+        title="Warning",
+        color=0xefbc2f,
+        icon="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678136-shield-warning-512.png"
+    )
+    ERROR = Format(
+        title="Error",
+        color=0xc64935,
+        icon="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678069-sign-error-512.png"
+    )
+    WORKING = Format(
+        title="Please Wait ...",
+        color=0x36393e,
+        icon="https://images-ext-1.discordapp.net/external/AzWR8HxPJ4t4rPA1DagxJkZsOCOMp4OTgwxL3QAjF4U/https/"
+             "cdn.discordapp.com/emojis/424900448663633920.gif"
+    )
+    WAITING = Format(
+        title="Waiting for Input ...",
+        color=0x478fce,
+        icon="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/bubbles-alt2-512.png"
+    )
 
     def format(self, content="", *, embed=None, f: Format = DEFAULT, **kwargs):
         formatted = {
